8493c0983c2f33c155af4a22462d346e5b423a09
Sonntd271 <itssonntd271@gmail.com>
2023-04-18 00:27:49 +0700

    hol up let me cook

--- sussy.py
+++ sussy.py
@@ -9,162 +9,172 @@ import json
 import time
 import threading
 
-gestures = {0:"do",1:"re",2:"mi",3:"fa",4:"sol",5:"la",6:"ti"}
-model = load_model("models/doremi.h5",compile= False)
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
-              loss=tf.keras.losses.BinaryCrossentropy(),
-              metrics=[tf.keras.metrics.BinaryAccuracy(),
-                       tf.keras.metrics.FalseNegatives()])
+SONG_FILE = "songs.json"
 
-def music_game(index,cap,song,frame,bool):
-    end = False
-    inc = False
-    cv2.putText(frame, song[index], (int(cap.get(3)/2), int(cap.get(4)/2)), cv2.FONT_HERSHEY_SIMPLEX, 1,
-                    (0,0,255), 2)
-    if bool == True:
-        index += 1
-        inc = True
-    if index == len(song):
-        end = True
-        index = 0
-        print("END")
-    return inc,end
 
-def choose_song():
-    song,notes = [[],[]]
-    with open("songs.json") as json_file:
-        data = json.load(json_file)
-        song = random.choice(list(data["songs"].items()))[1]
-        notes = random.choice(list(data["notes"].items()))[1]
-    return song,notes
+class Sussy:
 
-def play_sound(note,octave):
-    pygame.init()
-    print(f"playing {note}{octave}")
-    pygame.mixer.music.load(f"piano-mp3/piano-mp3/{note}{octave}.mp3")
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy():
-        pygame.time.Clock().tick(10)
-    pygame.quit()
+    def __init__(self):
+        
+        self.gestures = {0: "do", 1: "re", 2: "mi", 3: "fa", 4: "sol", 5: "la", 6: "ti"}
+        self.count = 0
+        self.index = 0
+        self.color = (0, 0, 255)
+        self.pred = "No"
+        self.prev_pred = None
 
-def parallel(note):
-    octave = note[1]
-    note = note[0]
-    thread = threading.Thread(target = play_sound,args = (note,octave))
-    thread.start()
+        self.model = load_model("models/doremi.h5", compile=False)
+        self.model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
+                    loss=tf.keras.losses.BinaryCrossentropy(),
+                    metrics=[tf.keras.metrics.BinaryAccuracy(),
+                            tf.keras.metrics.FalseNegatives()])
+        self.ht = HandTracking()
+        self.choose_song()
 
-def playback(song):
-    pygame.init()
-    for i in song:
-        pygame.mixer.music.load(f"piano-mp3/piano-mp3/{i}.mp3")
+    def music_game(self, index, cap, song, frame, bool):
+
+        end = False
+        inc = False
+        cv2.putText(frame, song[index], (int(cap.get(3)/2), int(cap.get(4)/2)), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                        (0,0,255), 2)
+        if bool == True:
+            index += 1
+            inc = True
+        if index == len(song):
+            end = True
+            index = 0
+            print("END")
+
+        return inc, end
+
+    def choose_song(self):
+
+        self.song, self.notes = [[], []]
+        with open(SONG_FILE) as json_file:
+            data = json.load(json_file)
+            self.song = random.choice(list(data["songs"].items()))[1]
+            self.notes = random.choice(list(data["notes"].items()))[1]
+
+    def play_sound(self, note, octave):
+
+        pygame.init()
+        print(f"playing {note}{octave}")
+        pygame.mixer.music.load(f"notes/{note}{octave}.mp3")
         pygame.mixer.music.play()
         while pygame.mixer.music.get_busy():
+            # Check the tick
             pygame.time.Clock().tick(10)
-    pygame.quit()
+        pygame.quit()
 
-def hands_feed():
-    HT = HandTracking()
+    def parallel(self, note):
+        
+        octave = note[1]
+        note = note[0]
+        thread = threading.Thread(target=self.play_sound, args=(note, octave))
+        thread.start()
 
-    count = 0
-    index = 0
-    color = (0, 0, 255)
+    def playback(self, song):
 
-    # capture from live web cam
-    cap = cv2.VideoCapture(0)
-    frame_width, frame_height = int(cap.get(3)), int(cap.get(4))
+        pygame.init()
+        for i in song:
+            pygame.mixer.music.load(f"notes/{i}.mp3")
+            pygame.mixer.music.play()
+            while pygame.mixer.music.get_busy():
+                pygame.time.Clock().tick(10)
+        pygame.quit()
 
-    song,notes = choose_song()
-    pred = "No predictions"
-    prev_pred = None
-    l = []
+    def interact(self, cap, frame):
+
+        self.insert = False
+
+        img = frame.copy()
+        rgb_frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        rgb_frame.flags.writeable = False
+
+        self.hands_results = self.ht.track(rgb_frame)
+        
+        rgb_frame.flags.writeable = True
+        bgr_frame = cv2.cvtColor(rgb_frame, cv2.COLOR_RGB2BGR)
+
+        self.ht.read_results(bgr_frame, self.hands_results)
+        
+        l = []
+        if self.hands_results.multi_hand_landmarks:
+            self.ht.draw_hand()
+            self.ht.draw_hand_label()
+
+            hand = self.hands_results.multi_hand_landmarks[0]
+            for i in range(21):
+                l += self.ht.get_moy_coords(hand, i)
+        else: 
+            self.pred = "No"
+        
+        # print(l)
+
+        if len(l) == 0:
+            self.pred = ""
+        else:
+            a = [l]
+            p = self.model.predict(a)    
+            p_index = np.argmax(p, axis=1)
+            self.pred = self.gestures[int(str(p_index)[1:-1])]
+        
+        if self.prev_pred == self.pred and self.pred != "" and self.pred == self.song[self.index]:
+            self.count += 1
+            if self.count >= 7:
+                self.color = (0,255,0)
+                self.count = 0
+                self.insert = True
+        else:
+            self.count = 0
+            self.prev_pred = None
+            self.color = (0,0,255)
+
+        if self.pred != "":
+            self.prev_pred = self.pred
+        
+        print("Want:", self.song[self.index])
+        print("Pred:", self.pred)
+        print("Prev:", self.prev_pred)
+        print("Count:", self.count)
+        print("Insert:", self.insert)
+        print("Index:", self.index)
+
+        inc, end = self.music_game(self.index, cap, self.song, bgr_frame, self.insert)
+
+        if inc:
+            self.parallel(self.notes[self.index])
+            self.index += 1
+
+        if end:
+            self.playback(self.notes)
+            #add stuff for resetting
+        
+        cv2.putText(bgr_frame, self.pred, (10, int(cap.get(4)) - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, self.color, 2)
+
+        return bgr_frame
+
+
+if __name__== "__main__":
     
+    cap = cv2.VideoCapture(0)
+    sus = Sussy()
+
     while cap.isOpened():
-        insert = False
+        # insert = False
         ret, frame = cap.read()
 
         if not ret:
             print("Ignoring empty camera frame.")
             continue
 
-        image = frame.copy()
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
+        frame = sus.interact(cap=cap, frame=frame)
 
-        # hand tracking
-        hands_results = HT.track(image)
+        cv2.imshow("Image", frame)
 
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-        # init frame each loop
-        HT.read_results(image, hands_results)
-
-        l = []
-        if hands_results.multi_hand_landmarks:
-            HT.draw_hand()
-            HT.draw_hand_label()
-
-            hand = hands_results.multi_hand_landmarks[0]
-            for i in range(21):
-                l += HT.get_moy_coords(hand, i)
-
-            # sl = ",".join(map(str, l))
-            # print(sl)
-        else: 
-            pred = "No predictions"
-
-        print(l)
-
-        if len(l) == 0:
-            pred = ""
-        else:
-            a = [l]
-            p = model.predict(a)    
-            p_index = np.argmax(p, axis=1)
-            pred = gestures[int(str(p_index)[1:-1])]
-        
-        # print("prev pred:", prev_pred)
-        # print("pred:",pred)
-        if prev_pred == pred and pred != "" and pred == song[index]:
-            count += 1
-            if count >= 7:
-                color = (0,255,0)
-                count = 0
-                insert = True
-        else:
-            count = 0
-            prev_pred = None
-            color = (0,0,255)
-
-        if pred != "No predictions":
-            prev_pred = pred
-
-        print(pred)
-        print(insert)
-        print(index)
-        inc,end = music_game(index,cap,song,image,insert)
-
-        if inc:
-            parallel(notes[index])
-            index += 1
-
-        if end:
-            playback(notes)
-            #add stuff for resetting
-
-        cv2.putText(image, pred, (10, frame_height - 10), cv2.FONT_HERSHEY_SIMPLEX, 1,
-                    color, 2)
-
-        cv2.imshow("image", image)
-
-        key = cv2.waitKey(100)
-
-        if key == ord("q"):
+        if cv2.waitKey(1) == ord("q"):
             break
 
     cap.release()
-    cv2.destroyAllWindows()
-
-if __name__== "__main__":
-    hands_feed()
+    cv2.destroyAllWindows
     
